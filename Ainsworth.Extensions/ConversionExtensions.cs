e0ffc0f5ea0643ac03f57117e609fd7ddd2e2838
Scott G. Ainsworth <scott@ainsworth.us>
2023-12-10 21:05:15 -0500

    Add Conversions extensions.

---
+++ Ainsworth.Extensions/ConversionExtensions.cs
@@ -0,0 +1,97 @@
+﻿using System.Numerics;
+using System.Security.Cryptography;
+
+namespace Ainsworth.Extensions;
+
+public static class ConversionExtensions {
+
+    #region Parse()
+
+    /// <inheritdoc cref="int.Parse(string)"/>
+    public static int ParseInt(this string s) => int.Parse(s);
+
+    /// <inheritdoc cref="int.Parse(string)"/>
+    /// <exception cref="InvalidCastException">
+    ///   <paramref name="obj"/> is not a <see cref="string"/>
+    /// </exception>
+    public static int ParseInt(this object? obj) => ((string)obj!).ParseInt();
+
+    /// <inheritdoc cref="long.Parse(string)"/>
+    public static long ParseLong(this string s) => long.Parse(s);
+
+    /// <inheritdoc cref="long.Parse(string)"/>
+    /// <exception cref="InvalidCastException">
+    ///   <paramref name="obj"/> is not a <see cref="string"/>
+    /// </exception>
+    public static long ParseLong(this object? obj) => ((string)obj!).ParseLong();
+
+    #endregion
+    #region Casts
+
+    /// <summary>Cast to a long.</summary>
+    /// <param name="value">The value to cast</param>
+    /// <exception cref="InvalidCastException">
+    ///   <paramref name="value"/> and integral type
+    ///   which can be converted to <see langword="long"/> without data loss.
+    /// </exception>
+    public static long ToLong(this sbyte value) => (long)value;
+
+    /// <summary>Cast to a long.</summary>
+    /// <param name="value">The value to cast</param>
+    /// <exception cref="InvalidCastException">
+    ///   <paramref name="value"/> and integral type
+    ///   which can be converted to <see langword="long"/> without data loss.
+    /// </exception>
+    public static long ToLong(this byte value) => (long)value;
+
+     /// <summary>Cast to a long.</summary>
+    /// <param name="value">The value to cast</param>
+    /// <exception cref="InvalidCastException">
+    ///   <paramref name="value"/> and integral type
+    ///   which can be converted to <see langword="long"/> without data loss.
+    /// </exception>
+   public static long ToLong(this short value) => (long)value;
+
+    /// <summary>Cast to a long.</summary>
+    /// <param name="value">The value to cast</param>
+    /// <exception cref="InvalidCastException">
+    ///   <paramref name="value"/> and integral type
+    ///   which can be converted to <see langword="long"/> without data loss.
+    /// </exception>
+    public static long ToLong(this ushort value) => (long)value;
+
+    /// <summary>Cast to a long.</summary>
+    /// <param name="value">The value to cast</param>
+    /// <exception cref="InvalidCastException">
+    ///   <paramref name="value"/> and integral type
+    ///   which can be converted to <see langword="long"/> without data loss.
+    /// </exception>
+    public static long ToLong(this int value) => (long)value;
+
+     /// <summary>Cast to a long.</summary>
+    /// <param name="value">The value to cast</param>
+    /// <exception cref="InvalidCastException">
+    ///   <paramref name="value"/> and integral type
+    ///   which can be converted to <see langword="long"/> without data loss.
+    /// </exception>
+   public static long ToLong(this uint value) => (long)value;
+
+    /// <summary>Cast to a long.</summary>
+    /// <param name="value">The value to cast</param>
+    /// <exception cref="InvalidCastException">
+    ///   <paramref name="value"/> and integral type
+    ///   which can be converted to <see langword="long"/> without data loss.
+    /// </exception>
+    public static long ToLong(this long value) => (long)value;
+
+    /// <summary>Cast to a long.</summary>
+    /// <param name="value">The value to cast</param>
+    /// <exception cref="InvalidCastException">
+    ///   <paramref name="value"/> and integral type
+    ///   which can be converted to <see langword="long"/> without data loss.
+    /// </exception>
+    public static long ToLong(this ulong value) => (long)value;
+
+    #endregion
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # dotnet-extensions
-Various/random .NET extension methods 
+
+Various/random .NET extension methods
+
+The motivation behind this class library is to expressiveness of functional code styles in C#. For example:
+
+```C#
+    // Instead of function calls, inside of function calls, inside of function calls, ...
+    var i = int.Parse(some_function_that_returns_a_string());
+    var l = (long)some_function_that_returns_an_int();
+
+    // We can use a chain-style that can be read more naturally
+    // (at least for those who use languages written left-to-right).
+    var i = some_function_that_returns_a_string().ParseInt();
+    var l = some_function_that_returns_an_int().ToLong();
+```
+
+## Example: ConversionExtensions
+
+| Extension Method | Underlying .NET library Implementation |
+|------------------|----------------------------------------|
+| `ParseInt(string)`  | [`Int32.Parse(string)`](https://learn.microsoft.com/en-us/dotnet/api/system.int32.parse#system-int32-parse(system-string)) |
+| `ParseInt(object)`  | [`Int32.Parse(string)`](https://learn.microsoft.com/en-us/dotnet/api/system.int32.parse#system-int32-parse(system-string)) |
+| `ParseLong(string)` | [`Int64.Parse(string)`](https://learn.microsoft.com/en-us/dotnet/api/system.int64.parse#system-int64-parse(system-string)) |
+| `ParseLong(object)` | [`Int64.Parse(string)`](https://learn.microsoft.com/en-us/dotnet/api/system.int64.parse#system-int64-parse(system-string)) |
+
+## Notes
+
+1. This is far from a complete library. Basically, I add new features as I need them. Few, if any, of the possibilites in the standard .NET libraries are covered completely. For instance, [`int.Parse()`](https://learn.microsoft.com/en-us/dotnet/api/system.int32.parse) has has eigth overloads. This library currently covers just one.
