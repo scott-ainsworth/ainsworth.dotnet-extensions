e6c8b15fb62c6db4e8c1a16ab2afea3ce74aee8a
Scott G. Ainsworth <scott@ainsworth.us>
2024-10-14 19:16:34 -0400

    chore: Stabilize GitVersion operations
    - Modify `gitversion.sh` to fix line spacing on 'update' as it already does on 'clean'.
    - Update project targets so that `gitversion.sh` is run once per dotnet command.
    - Make GitVersion operations work properly when run from VSCode.

--- util/gitversion.sh
+++ util/gitversion.sh
@@ -13,43 +13,60 @@
 set -o nounset
 set -o errexit
 
+##
+# fix csproj spacing that dotnet-gitversion messes up (stdin -> stdout)
+function fixup_csproj_spacing() {
+    gawk '
+        # Put back blank lines removed by dotnet-gitversion
+        /^\s*<PropertyGroup>\s*$/ { print "" }
+        /^\s*<ItemGroup>\s*$/ { print "" }
+        /^\s*<Target .*>\s*$/ { print "" }
+        /^\s*<\/Project>\s*$/ { print "" }
+
+        # copy all lines not a version
+        { print }
+    ' | uniq
+}
+
+##
+# Rename if csproj changed
+# $1 = original csproj file
+# $2 = re-spaced csproj file
+function roll_csproj() {
+    cmp -s "$1" "$2" && rm "$2" || mv "$2" "$1"
+}
+
 VERSION_FILE='AssemblyVersionInfo.cs'
 SCRIPT="$(basename "$0")"
 
-echo "$SCRIPT $*"
-
-#set | sed "s/^/$SCRIPT: /"
-
 if [[ "$1" == "update" ]]; then
 
+    # restore dotnet-gitversion tool
     dotnet tool restore
+
+    # update the project files and show the version
     echo "InformationalVersion -> $(dotnet tool run dotnet-gitversion /showvariable InformationalVersion /updateprojectfiles)"
+    for csproj in *.csproj; do
+        cat "$csproj" | fixup_csproj_spacing >"$csproj.$$"
+        roll_csproj "$csproj" "$csproj.$$"
+    done
 
 elif [[ "$1" == "clean" ]]; then
 
+    # clean all projects (usually just one)
     for csproj in *.csproj; do
-        echo "$SCRIPT: clean $csproj"
         gawk '
-            # Skip version lines
-            /^\s*<AssemblyVersion>.*<\/AssemblyVersion>\s*$/ { next }
-            /^\s*<FileVersion>.*<\/FileVersion>\s*$/ { next }
-            /^\s*<InformationalVersion>.*<\/InformationalVersion>\s*$/ { next }
-            /^\s*<Version>.*<\/Version>\s*$/ { next }
-
-            # Put back blank lines removed by dotnet-gitversion
-            /^\s*<PropertyGroup>\s*$/ { print "" }
-            /^\s*<ItemGroup>\s*$/ { print "" }
-            /^\s*<Target .*>\s*$/ { print "" }
-            /^\s*<\/Project>\s*$/ { print "" }
-
-            # copy all lines not a version
+          # Skip version lines
+            /^\s*<AssemblyVersion>.*<\/AssemblyVersion>\s*$/ ||
+            /^\s*<FileVersion>.*<\/FileVersion>\s*$/ ||
+            /^\s*<InformationalVersion>.*<\/InformationalVersion>\s*$/ ||
+            /^\s*<Version>.*<\/Version>\s*$/ {
+              next
+            }
+          # Copy everything else
             { print }
-        ' "$csproj" | uniq >"$csproj.$$"
-        if cmp -s "$csproj" "$csproj.$$"; then
-            rm "$csproj.$$"
-        else
-            mv "$csproj.$$" "$csproj"
-        fi
+        ' "$csproj" | fixup_csproj_spacing > "$csproj.$$"
+        roll_csproj "$csproj" "$csproj.$$"
     done
 
 else
