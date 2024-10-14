4b2f60c14f07d73eebb74a1bec797a1b45e121ee
Scott G. Ainsworth <scott@ainsworth.us>
2024-10-14 11:01:08 -0400

    style: Add `IsExternInit`
    `IsExternalInit` enables C# 9 records and property init accessor.

---
+++ Ainsworth.Extensions/System.Runtime.CompilerServices/IsExternalInit.cs
@@ -0,0 +1,29 @@
+// Copied from https://github.com/dotnet/runtime/blob/main/src/libraries/System.Private.CoreLib/src/System/Runtime/CompilerServices/IsExternalInit.cs
+
+#if !NET5_0_OR_GREATER
+
+#define SYSTEM_PRIVATE_CORELIB
+
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.ComponentModel;
+
+namespace System.Runtime.CompilerServices
+{
+    /// <summary>
+    /// Reserved to be used by the compiler for tracking metadata.
+    /// This class should not be used by developers in source code.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+#if SYSTEM_PRIVATE_CORELIB
+    public
+#else
+    internal
+#endif
+    static class IsExternalInit
+    {
+    }
+}
+
+#endif
